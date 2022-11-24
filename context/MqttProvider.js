3043d9326c8e78458e1054c90d99da16862ab17b
sarbjit singh <sarbjitsingh@sarbjits-MacBook-Air.local>
2022-11-24 15:26:05 +0000

    update mqttprovider to make mqtt reconnect

--- context/MqttProvider.js
+++ context/MqttProvider.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 //import { connect } from "precompiled-mqtt";
 import { connect } from "mqtt";
 import _ from "lodash";
@@ -10,57 +10,73 @@ export default function MqttProvider({ brokerConfig, children }) {
   const [client, setClient] = React.useState(null);
   const [messages, setMessages] = React.useState({});
   const [finalData, setFinalData] = React.useState([]);
+  const [reconnect, setReconnect] = React.useState("");
   const { login, user, isAuthenticated, loading } = useAuth();
+  const ref_client = useRef();
 
-  if (client == null && user?.id) {
-    const newClient = connect(brokerConfig.url, brokerConfig.options);
-    //const newClient = connect(brokerConfig.url);
-    newClient.on("connect", () => {
-      //subcribe topic
-      newClient?.subscribe(`/outTopic/${user?.id}`);
-      console.log("user id=", `/outTopic/${user?.id}`);
-      console.log("new client connected");
-    });
+  //mqtt connection request
+  const connectionRequest = () => {
+    if (user?.id && !client?.connected) {
+      const newClient = connect(brokerConfig.url, brokerConfig.options);
+      //const newClient = connect(brokerConfig.url);
+      newClient.on("connect", () => {
+        //subcribe topic
+        newClient?.subscribe(`/outTopic/${user?.id}`);
+        console.log("user id=", `/outTopic/${user?.id}`);
+        console.log("new client connected", newClient);
+        setClient(newClient);
+      });
+      newClient.on("disconnect", () => {
+        console.log("new client disconnected");
+        setClient(null);
+      });
 
-    newClient.on("disconnect", () => {
-      console.log("new client disconnected");
-      setClient(null);
-    });
+      newClient.on("error", (err) => {
+        setClient(null);
+        console.log("error in connection", err);
+      });
+      newClient.on("close", () => {
+        newClient?.end();
+        setClient(null);
+        console.log("close connection");
+      });
+      newClient.on("message", (topic, message, packet) => {
+        try {
+          const json = JSON.parse(new TextDecoder("utf-8").decode(message));
+          //verify recived data is correct
+          const { gateway, node } = json;
+          if (gateway == undefined || node == undefined)
+            throw new Error("invaild json data received");
+          //enter time stamping with received data
+          json["time"] = Date.now();
+          setMessages(json);
+        } catch (err) {
+          console.log("data error -> ", err.message);
+        }
+      });
+    }
+  };
+  useEffect(() => {
+    if (client != null) {
+      ref_client.current = client;
+    }
+  }, [client]);
 
-    newClient.on("error", (err) => {
-      console.log("error in connection", err);
-    });
-    newClient.on("close", () => {
-      newClient?.end();
-      console.log("close connection");
-    });
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (!client?.connected)
+        //just randomly change the reconnect string value to enable to run loop again
+        setReconnect(Date.now().toString());
+    }, 5000);
+    return () => clearInterval(timer);
+  }, [client]);
 
-    //   client?.removeAllListeners();
-    //   newClient?.removeAllListeners();
-    //   newClient?.end();
-    //   console.log("close connection", err);
-    // });
-    newClient.on("message", (topic, message, packet) => {
-      try {
-        const json = JSON.parse(new TextDecoder("utf-8").decode(message));
-        //verify recived data is correct
-        const { gateway, node } = json;
-        if (gateway == undefined || node == undefined)
-          throw new Error("invaild json data received");
-        //enter time stamping with received data
-        json["time"] = Date.now();
-        setMessages(json);
-      } catch (err) {
-        console.log("data error -> ", err.message);
-      }
-    });
-
-    setClient(newClient);
-  }
+  useEffect(() => {
+    connectionRequest();
+  }, [reconnect]);
 
   //create timer
   useEffect(() => {
-    const currentTime = Date.now().toString();
     const timer = setInterval(() => {
       //remove item from array if time difference is greater than 30s
       if (finalData.length > 0) {
@@ -94,8 +110,21 @@ export default function MqttProvider({ brokerConfig, children }) {
     filter();
   }, [messages]);
 
+  const publish_data = (msg) => {
+    try {
+      ref_client?.current?.publish(
+        `/inTopic/${user?.id}`,
+        JSON.stringify(msg),
+        0,
+        false
+      );
+    } catch (err) {
+      console.log("error to publish mqtt", err.message);
+    }
+  };
+
   return (
-    <MqttContext.Provider value={{ client, finalData }}>
+    <MqttContext.Provider value={{ client, finalData, publish_data }}>
       {children}
     </MqttContext.Provider>
   );
